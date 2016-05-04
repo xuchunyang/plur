5bdd3b9a2f0624414bd596e798644713cd1545f0
Chunyang Xu <xuchunyang.me@gmail.com>
2016-05-04 17:24:56 +0800

    Beep version

--- plur.el
+++ plur.el
@@ -4,7 +4,7 @@
 
 ;; Author: Chunyang Xu <xuchunyang.me@gmail.com>
 ;; URL: https://github.com/xuchunyang/plur
-;; Version: 0.01
+;; Version: 0.1
 ;; Package-Requires: ((emacs "24.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
